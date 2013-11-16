358c2af716c7f04a9e7f98e2af945fc2d926eda7
Jun Huang <mightybeesun@gmail.com>
2013-11-16 11:12:51 -0500

    restructure file struct

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AGL::Application.config.secret_token = 'd1aa4856e331a64f6d1473cc7ad0082b6f0b98103755a81da200875d0b95a32d66a92f64a8d51ffa2d719e51aea487e70c1aaae5f2b787f8a649d59290d3aa9a'
