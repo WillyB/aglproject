c7802dfe41c54de66fa98b7b0482fc551fd5048f
vagrant <vagrant@precise32.(none)>
2013-11-16 01:34:04 +0000

    Initial check in of new rails app , rails version 3.214

---
+++ AGL/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AGL::Application.config.secret_token = 'ffe7f8912aa37cbbde8d1d2f2d6530c0367b5971e85a4f3d5c05ceebb85f67425dbc68de50edb62ee8bd7ff7f494b7b5ba18cc31120dfdff82d616acbf1b0c87'
